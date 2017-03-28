7b67ddbdc75059789449ea04b13f3f6f5a288c8b
Brandon Cooper and Jiwon Kang <brandon+jiwon@>
2017-03-28 13:39:14 -0700

    adding Done button that sets selectedRecipe variable back to a null value so that it hides the editing form, addingEdit button that displays editing form setting the selectedRecipe variable to clickedRecipe value

--- app/app.component.ts
+++ app/app.component.ts
@@ -6,10 +6,10 @@ import { Component } from '@angular/core';
    <div class="container">
      <h1>Recipe Box</h1>
      <ul>
-      <li [class]="priorityColor(currentRecipe)" (click)="editRecipe(currentRecipe)" *ngFor="let currentRecipe of recipes">{{currentRecipe.name}} - {{currentRecipe.ingredient}} <button (click)="editRecipe(currentRecipe)">Edit</button></li>
+      <li [class]="priorityColor(currentRecipe)" (click)="isCooked(currentRecipe)" *ngFor="let currentRecipe of recipes">{{currentRecipe.name}} - {{currentRecipe.ingredient}} <button (click)="editRecipe(currentRecipe)">Edit</button></li>
     </ul>
     <hr>
-    <div>
+    <div *ngIf="selectedRecipe">
      <h3>{{selectedRecipe.name}}</h3>
      <p>Recipe Complete? {{selectedRecipe.cooked}}</p>
     <h3>Edit Recipe</h3>
@@ -19,7 +19,8 @@ import { Component } from '@angular/core';
      <br>
      <input type="radio" [(ngModel)]="selectedRecipe.priority" [value]="1">1 (Low Priority)<br>
      <input type="radio" [(ngModel)]="selectedRecipe.priority" [value]="2">2 (Medium Priority)<br>
-     <input type="radio" [(ngModel)]="selectedRecipe.priority" [value]="3">3 (High Priority)
+     <input type="radio" [(ngModel)]="selectedRecipe.priority" [value]="3">3 (High Priority)<br>
+     <button (click)="finishedEditing()">Done</button>
     </div>
    </div>
   `
@@ -31,7 +32,8 @@ export class AppComponent {
     new Recipe('Nachos', 'cheese, chips', 2)
   ];
 
-  selectedRecipe: Recipe = this.recipes[0];
+  // selectedRecipe: Recipe = this.recipes[0];
+  selectedRecipe = null;
 
   editRecipe(clickedRecipe) {
     this.selectedRecipe = clickedRecipe;
@@ -46,14 +48,18 @@ export class AppComponent {
   }
 
   priorityColor(currentRecipe){
-  if (currentRecipe.priority === 3){
-    return "bg-danger";
-  } else if (currentRecipe.priority === 2) {
-    return  "bg-warning";
-  } else {
-    return "bg-info";
+    if (currentRecipe.priority === 3){
+      return "bg-danger";
+    } else if (currentRecipe.priority === 2) {
+      return  "bg-warning";
+    } else {
+      return "bg-info";
+    }
   }
-}
+
+  finishedEditing() {
+    this.selectedRecipe = null;
+  }
 }
 
 export class Recipe {
